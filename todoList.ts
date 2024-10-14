e1f4615e41e5ea20b71ed726cbacaa2ca9f8ce6e
CasimirG12 <casimirbomans@gmail.com>
2024-10-14 10:52:03 +0200

    I was bored so I wrote some code

--- todoList.ts
+++ todoList.ts
@@ -3,10 +3,22 @@ import { TodoItem } from "./models";
 const input = require('prompt-sync')({sigint: true});
 
 export class TodoList {
-  private idCounter: number;
-  private todoList: TodoItem[];
+  idCounter
+  todoList
   constructor () {
     this.idCounter = 0;
     this.todoList = [];
+  }
+  
+  addItem = () => {
+
+  }
+
+  deleteItem = () => {
+
+  }
+
+  viewList = () => {
+    
   }
 }
