6b4a5cb3892d401ee240f6642db8897732a08dc4
CasimirG12 <casimirbomans@gmail.com>
2024-10-14 10:43:36 +0200

    create todo list class, closes #3

---
+++ todoList.ts
@@ -0,0 +1,12 @@
+import { TodoItem } from "./models";
+
+const input = require('prompt-sync')({sigint: true});
+
+export class TodoList {
+  private idCounter: number;
+  private todoList: TodoItem[];
+  constructor () {
+    this.idCounter = 0;
+    this.todoList = [];
+  }
+}
